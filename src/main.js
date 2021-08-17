27ec821493b0caf7cd25ebefd5e191dd793d5620
hcq_pm <hcq_pm@163.com>
2021-08-17 14:07:48 +0800

    进度条插件

--- src/main.js
+++ src/main.js
@@ -3,9 +3,19 @@ import App from './App.vue'
 import store from './store/index'
 import VueRouter from 'vue-router'
 import router from './router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+
+NProgress.configure({
+  easing: 'ease',// 动画方式
+  speed: 500,// 递增进度条的速度
+  showSpinner: false, // 是否显示加载ico
+  trickleSpeed: 200,// 自动递增间隔
+  minimum: 0.3// 初始化时的最小百分比
+})
 
 new Vue({
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import tutorialRouter from './modules/tutorial'
 import aboutRouter from './modules/about'
 import detailRouter from './modules/detail'
 import userRouter from './modules/user'
+import nProgress from 'nprogress'
 
 const router = new VueRouter({
     routes: [
@@ -15,7 +16,12 @@ const router = new VueRouter({
         userRouter
     ]
 })
+router.beforeEach((to, from, next) => {
+    nProgress.start()
+    next()
+})
 router.afterEach(to => {
+    nProgress.done()
     document.title = to.meta.title || "CNode";
 })
 export default router
